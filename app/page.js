70cc948ef942ff4c94ec9b6b19cc0e9c3b88f5bc
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2024-01-17 15:44:47 +0300

    Created home page

--- app/page.js
+++ app/page.js
@@ -1,95 +1,64 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-export default function Home() {
+const Banner = ({ purpose, title1, title2, description1, description2, buttonText, linkName, imageURL  }) => { // Creating a reusable banner component. Every component has props, which are the properties that are passed in. in this case we have a prop called purpose.
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+      {/* {purpose} This is the text that will be passed in. it's a reusable component */}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Image src={imageURL} width={500} height={300} />
+      <Box p="5"> {/*P is padding*/}
+        
+        <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+        <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
+        <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{description1}<br />{description2}</Text>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <Button fontSize='xl' bg="blue.300" color="white">
+          <Link href={linkName}>{buttonText}</Link>
+        </Button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      </Box>
+    </Flex>
+  );
+};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+const Home = ({ propertiesForSale, propertiesForRent }) => {
+  return (
+    <Box>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Banner 
+        purpose={ 'RENT A HOME' } 
+        title1={ 'Rental Homes For' }
+        title2={ 'Everyone' }
+        description1={ 'Explore from Apartments, builder floors, villas' }
+        description2={ 'and more.' }
+        buttonText={ 'Explore Renting' }
+        linkName={ '/search?purpose=for-rent' }
+        imageURL={ 'https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4' }
+      />
+
+      {/* <Flex flexWrap='wrap'>
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex> */}
+
+      <Banner 
+        purpose={ 'BUY A HOME' }
+        title1={ ' Find, Buy & Own Your' }
+        title2={ 'Dream Home' }
+        description1={ ' Explore from Apartments, land, builder floors,' }
+        description2={ ' villas and more' }
+        buttonText={ 'Explore Buying' }
+        linkName={ '/search?purpose=for-sale' }
+        imageURL={ 'https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008' }
+      />
+
+      {/* <Flex flexWrap='wrap'>
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex> */}
+
+    </Box>
+  );
 }
+
+export default Home;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const { clear } = require('console');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'bayut-production.s3.eu-central-1.amazonaws.com'
+        },
+      ],
+    },
+  }
